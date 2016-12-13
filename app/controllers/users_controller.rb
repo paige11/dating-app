5a59b1e2bb3d16dcdd3ee6c15880715d89e40262
paige11 <ellis.paige.v@gmail.com>
2016-12-13 16:13:12 -0500

    Initial Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @matches = Match.where(personality_type_id: @user.personality_type)
+  end
+
+  def index
+    @users = User.all
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
+  skip_before_action :authenticate_user!
 
   def home
     if current_user && current_user.personality.nil?
